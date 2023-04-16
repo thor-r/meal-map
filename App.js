5876b724dfeb118764ce50d38f2ab28a5e025894
thor-r <twilliamrefoy@gmail.com>
2023-04-16 22:24:12 +0100

    day 1

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView 
+      style={{
+        flex: 1,
+        backgroundColor: 'red' 
+        }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'purple'
+      }}>
+      <Text>Search</Text>
     </View>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'blue'
+      }}>
+        <Text>list</Text>
+    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ README.md
@@ -0,0 +1,6 @@
+Commands to start app
+
+open -a Simulator - initializes the simulator 
+expo start - equivalent of yarn start
+yarn { ios, web, android } - shortcut to start app on device
+Select device to emulate
